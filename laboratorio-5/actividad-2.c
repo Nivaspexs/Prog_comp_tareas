68111053a201a1021451a1922d4ef3a015750493
Nivaspexs <n.vasquez06@ufromail.cl>
2021-09-29 14:46:06 -0300

    Lab 5 tarea

--- laboratorio-5/actividad-2.c
+++ laboratorio-5/actividad-2.c
@@ -2,14 +2,21 @@
 
 int main(){
 
-    printf("\n El programa trata de ingresar un caracter para que después se muestre con su código ASCII");
+    printf("\n El programa trata de ingresar un caracter para que después se muestre con su código ASCII\n\n");
+
+    /*
+    Definir variable "carac" que es el caracter que se pide después
+    */
 
     char carac;
 
-    printf("Ingrese un caracter para que se muestre su codigo ASCII\n");
+    printf("Ingrese un caracter para que se muestre su codigo ASCII:  ");
     scanf("%c", &carac);
 
-    printf("El codigo ASCII de %c es %i\n", carac, carac);
+    /*
+    Indicando al programa que carac pasa a ser un int (ocupando %i) muestra directamente el código ASCII del caracter
+    */
+    printf("\nEl codigo ASCII de %c es %i\n", carac, carac);
 
 
 
