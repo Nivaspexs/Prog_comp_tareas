6eb3e391a4bd33e3a4ecce087a75fe6d7422cea8
Nivaspexs <n.vasquez06@ufromail.cl>
2021-09-29 15:59:54 -0300

    Lab 5 tarea

---
+++ laboratorio-5/actividad-4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    printf("El programa consiste en obtener el perímetro y el área de un círculo en base al radio que el usuario ingresa\n");
+
+
+    int radio;
+    float perimetro;
+    float area;
+
+    printf("Ingrese el radio de la circunferencia: ");
+    scanf("%i", &radio);
+
+    perimetro = 2 * M_PI * radio ;
+    area = M_PI * radio * radio ;
+
+    printf("\nEl perímetro es %f y el área es %f", perimetro, area);
+
+
+    printf("%f\n", M_PI);
+
+
+
+
+
+
+
+    return 0;
+}
