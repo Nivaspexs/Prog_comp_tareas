80ad6ee64f6533b7d96c54c3bccd4e15946d462b
Nivaspexs <n.vasquez06@ufromail.cl>
2021-09-29 16:46:49 -0300

    Lab 5 tarea

--- laboratorio-5/actividad-5.c
+++ laboratorio-5/actividad-5.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+
+int main(){
+
+    float prueba1;
+    float practica1;
+    float practica2;
+    float practica3;
+    float prueba2;
+
+    printf("\nEste programa trata de calcularte la nota que se necesita en la Prueba 2 para pasar la asignatura, para ello tienes que colocar tus otras notas\n ");
+
+    printf("Ingrese nota de la primera prueba: ");
+    scanf("%f", &prueba1);
+    printf("\nIngrese las tres notas de la parte teórica: ");
+
+    scanf("%f", &practica1), printf(" , "), scanf("%f", &practica2), printf(" y "), scanf("%f", &practica3);
+
+
+
+
+    printf("%f , %f y %f", practica1, practica2, practica3);
+
+/*
+NF = (EvT1*0.5+EvT2*0.5)*0.3+(EvP1*0.2+EvP2*0.2+EvP3*0.6)*0.7
+*/
+
+
+ 
+
+
+
+
+
+
+
+
+    return 0;
+}
