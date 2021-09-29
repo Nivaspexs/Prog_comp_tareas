02cd03179ebc7ea6cd243c460f702be7eca11fa9
Nivaspexs <n.vasquez06@ufromail.cl>
2021-09-28 23:19:37 -0300

    Lab 5 tarea

---
+++ laboratorio-5/actividad-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+ /* 
+ Se muestra por pantalla la descripción del programa
+ */
+    printf("\n ¡Bienvenido a este simple programa! \n\n ");
+    printf("Aquí tienes que ingresar dos números para que se sumen\n\n");
+    /*
+    Se declaran las variables que se van a ocupar (números a sumar)
+    */
+
+    int num1;
+    int num2;
+    int resul;
+    /*
+    Se imprime por pantalla para pedir los números y después sumarlos e imprimir el resultado
+    */
+
+    printf(" Ingrese primer número:  ");
+    scanf("%i", &num1);
+    printf("\n Ingrese segundo número: ");
+    scanf("%i", &num2);
+
+    resul = num1 + num2 ;
+
+
+    printf("\n El resultado de la suma es %i\n\n", resul);
+
+    /*
+    Nt: Espaciar con \n fue esclusivamente para ordenar el programa
+    */
+
+return 0 ;
+}
