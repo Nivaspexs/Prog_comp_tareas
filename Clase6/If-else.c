934c901a393bb37f5a12cc420c7e8826e23e2e30
Nivaspexs <n.vasquez06@ufromail.cl>
2021-10-01 15:53:06 -0300

    Clase6

---
+++ Clase6/If-else.c
@@ -0,0 +1,30 @@
+int main (){
+
+    int n1,n2,suma;
+
+    printf("\nDebe ingresar dos números: ");
+    scanf("%i",&n1), printf(" y "), scanf("%i",&n2);
+
+    printf("\n%i y %i\n", n1,n2);
+
+    if (n2 > n1){
+        suma = n1+n2;
+        printf("\n% Numero2 mayor que numero1\n");
+
+
+        
+        }
+    
+    else {
+        printf("Numero1 mayor que numero2\n");
+
+
+        }
+
+    
+
+
+
+
+ return 0;}
+
---
+++ Clase6/If.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+
+    int n1,n2,suma;
+
+    printf("\nDebe ingresar dos números: ");
+    scanf("%i",&n1), printf(" y "), scanf("%i",&n2);
+
+    printf("\n%i y %i\n", n1,n2);
+
+    if (n2 == 1){
+        suma = n1+n2;
+        printf("\n%El valor de la suma es ½i\n", suma);
+
+
+        
+        }
+    
+
+
+ return 0;}
+
+
+
